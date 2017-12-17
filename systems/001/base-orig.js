2471a0258740c1e567f8711ef2e1ac2206c43fa8
gs-morgan <33827791+gs-morgan@users.noreply.github.com>
2017-12-17 17:55:29 +0700

    Add files via upload

--- systems/001/base-orig.js
+++ systems/001/base-orig.js
@@ -5,6 +5,15 @@
 	};
 	var blockstate = '0';
 	var webid = parseInt(window.location.search.replace(/\D+/g,""));
+	var mobileToster = '0';
+	//window;
+	if($(window).width() > 1050) {		
+			var setwind1 = document.getElementById('cl1');
+			setwind1.style.width =  (500 + setwind1.offsetWidth - setwind1.clientWidth) + "px";
+			var setwind2 = document.getElementById('cl2');
+			setwind2.style.width =  (500 + setwind2.offsetWidth - setwind2.clientWidth) + "px";
+			mobileToster = 1;		
+		}else{}
 	
 $.getJSON('https://spreadsheets.google.com/feeds/list/18t8PJW8ObN-LFlsC4PZEmyBR0DKW9PnD7OV66IZIwIg/od6/public/values?alt=json', function(data) {
   //$(".container-right").prepend('<div class="top-block"><h1>На данный момент, общее количество ресурсов: ' + data.feed.entry[0]['gsx$amountres']['$t'] + ' </div>');
@@ -19,7 +28,7 @@ $.getJSON('https://spreadsheets.google.com/feeds/list/18t8PJW8ObN-LFlsC4PZEmyBR0
 	  if (!!char_id && !!char_name) {
 			$(".submenu").append('<a href="?id=' + char_id + '"><div id="char-' + char_id + '" class="static-block"><h1 class="name">' + char_name + '</h1></div></a>');	
 			if(blockstate == '0' && webid == char_id) {
-				$(".container-right").append('<div id="char-' + char_id + '" class="static-block mobile"><h1 class="name">' + char_name + '</h1></div>');
+				$(".states").append('<div id="char-' + char_id + '" class="static-block mobile"><h1 class="name">' + char_name + '</h1></div>');
 				charstate = '1';
 			}else {};	
 	}else{
@@ -56,7 +65,7 @@ $.getJSON('https://spreadsheets.google.com/feeds/list/18t8PJW8ObN-LFlsC4PZEmyBR0
     var roll1 = entry['gsx$roll1']['$t'];	
     var roll2 = entry['gsx$roll2']['$t'];	
 	if (!!char_sid && !!skill && !!type && !!about && !!roll1 && roll2 && (window.location.search == '?id=' + char_sid) && ((type == 'dipl') || (type == 'atck'))) {
-			$(".container-right").append('<div class="' + type + '"><h1 class="title">' + skill + '</h1><p class="about">' + about + '</p><p class="roll-1">Ролл: 1-' + roll1 + '</p><p class="roll-2">Порог: ' + roll2 + '</p></div>');
+			$(".states").append('<div class="' + type + '"><h1 class="title">' + skill + '</h1><p class="about">' + about + '</p><p class="roll-1">Ролл: 1-' + roll1 + '</p><p class="roll-2">Порог: ' + roll2 + '</p></div>');
 			if(blockstate == '0') {
 				activeblock(char_sid);
 				blockstate = '1';
