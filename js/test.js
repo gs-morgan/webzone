a849796fc09feb49d13d7155e89a8398b6365d78
gs-morgan <33827791+gs-morgan@users.noreply.github.com>
2018-07-06 05:41:21 +0700

    fast

--- js/test.js
+++ js/test.js
@@ -2,3 +2,4 @@ $('.f_approvenickname .value').each(function() {
     var text = $(this).text();
     $(this).html(text.replace('Катя-Катерина ♡', '<a href="https://rp-wow.ru/users/94">Катя-Катерина ♡</a>')); 
 });
+
