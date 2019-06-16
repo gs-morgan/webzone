4a6300c8c241d4984f1d2347eec2652e0090b2c7
gs-morgan <33827791+gs-morgan@users.noreply.github.com>
2019-06-17 06:31:55 +0700

    Add files via upload

---
+++ alter/js/displer.js
@@ -0,0 +1,68 @@
+$.getJSON( './data3.json', function( data ) {
+	  
+		new Vue({
+			el: '#table',
+			data () {
+				return {
+					keyword: '',
+					dataArray: data,
+					fields: [
+						{key: 'ID', label: 'ID', sortable: true},
+						{key: 'Name_lang', label: 'NPC Name', sortable: true},
+						{key: 'DisplayID0', label: 'DisplayID[0]', sortable: true},
+						{key: 'DisplayID1', label: 'DisplayID[1]', sortable: false},
+						{key: 'DisplayID2', label: 'DisplayID[2]', sortable: false},
+						{key: 'DisplayID3', label: 'DisplayID[3]', sortable: false}
+					],
+					totalRows: 1,
+					currentPage: 1,
+					perPage: 15,
+					pageOptions: [5, 10, 15, 20, 25],
+					filter: null
+				}
+			},
+			computed: {
+				items () {
+					return this.keyword
+						? this.dataArray.filter(item => item.ID.includes(this.keyword)  || item.Name_lang.includes(this.keyword))
+						: this.dataArray
+						
+						this.totalRows = this.dataArray.length
+				}
+			},
+			mounted() {
+			  // Set the initial number of items
+			  this.totalRows = this.items.length
+			},
+			methods: {
+			  onFiltered(filteredItems) {
+				// Trigger pagination to update the number of buttons/pages due to filtering
+				this.totalRows = filteredItems.length
+				this.currentPage = 1
+			  }
+			}
+		});
+		
+	$('.disperer').on('click', function() {
+		var disp = $(this).text();
+		
+		if ((disp == 0) || disp == false) {
+			
+		}else {
+
+
+			var win, doc;
+
+			window.open('./disp.html?disp='+disp, "hello", "width=350,height=400");
+
+
+
+
+						
+						 
+
+					}
+			
+	});
+	});
+	
